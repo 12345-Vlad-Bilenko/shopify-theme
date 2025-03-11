51bde09cd85398e3a77863cebca5634c8cb54fbc
12345-Vlad-Bilenko <12345.vlad.b@gmail.com>
2025-03-11 17:39:07 +0200

    fix js

--- assets/custom.js
+++ assets/custom.js
@@ -1,9 +1,12 @@
-$(document).ready(function() {
-    $('.card__content').on('mouseenter', function() {
-      $('#fixed-rectangle').addClass('hovered');
+// Додаємо слухач подій для усіх елементів з класом card__content
+document.querySelectorAll('.card__content').forEach(function(item) {
+    item.addEventListener('mouseenter', function() {
+      // Додаємо клас до прямокутника при наведенні
+      document.getElementById('fixed-rectangle').classList.add('hovered');
     });
   
-    $('.card__content').on('mouseleave', function() {
-      $('#fixed-rectangle').removeClass('hovered');
+    item.addEventListener('mouseleave', function() {
+      // Видаляємо клас при відведенні курсора
+      document.getElementById('fixed-rectangle').classList.remove('hovered');
     });
   });
